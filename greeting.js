d80830af964c127be75e60a7b64f55a7137680ed
skawnkk <65053955+skawnkk@users.noreply.github.com>
2020-09-22 15:21:30 +0900

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,57 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+
+const greeting = document.querySelector(".js-greeting");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+
+// function enterkey() {
+//     if (input.event.keyCode == 13) {
+//         loadName();
+//     }
+// }
+
+function saveName(nameValue){
+    localStorage.setItem(USER_LS,nameValue);
+}
+
+function makeGreeting(nameValue){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText=`hello ${nameValue}`;
+    saveName(nameValue);
+}
+
+function stayName(event){
+    event.preventDefault();
+    const nameValue = input.value;
+    makeGreeting(nameValue);
+}
+
+function submitName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit",stayName);
+}
+
+
+
+function loadName(){
+    
+    const currentUser = localStorage.getItem(USER_LS);
+
+    if(currentUser === null){
+        submitName();
+
+    } else{
+        makeGreeting(currentUser);
+  
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
